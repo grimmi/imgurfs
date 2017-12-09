13ae92b8b03a62c7a492f1ebaa973e5276a9a8b3
Arne Lehmann <arne.lehmann@gmx.de>
2017-12-09 01:30:03 +0100

    refactored logic into small functions

--- ImgurFS/Script.fsx
+++ ImgurFS/Script.fsx
@@ -6,14 +6,14 @@ open System.IO
 
 let clientID = File.ReadAllLines @"c:\temp\tdd.txt" |> Seq.head |> String.filter(fun c -> not(c.Equals('#')))
 
-let saveStream (targetPath:string) (fileName:string) (index:int) (response:HttpResponseWithStream) =
+let SaveStreamFromHttpResponse (targetPath:string) (fileName:string) (index:int) (response:HttpResponseWithStream) =
     let name = sprintf "%03i - %s" index (Array.last (fileName.Split('/')))
     let targetFile = Path.Combine(targetPath, name)
     use saveStream = File.Create(targetFile)
     response.ResponseStream.CopyTo(saveStream)
     printfn "%s" targetFile
 
-let downloadImages targetPath (images : seq<string>) =
+let DownloadLinks targetPath (images : seq<string>) =
     
     if not (Directory.Exists targetPath) then (Directory.CreateDirectory(targetPath) |> ignore)
 
@@ -23,29 +23,31 @@ let downloadImages targetPath (images : seq<string>) =
     images
     |>Seq.map(fun image -> (image, Http.RequestStream(image, httpMethod = "GET")))
     |>Seq.iter(fun (image, stream) -> printf "[%d / %d] " counter imageLength
-                                      saveStream targetPath image counter stream
+                                      SaveStreamFromHttpResponse targetPath image counter stream
                                       counter <- counter + 1)
 
-let sanitizeAlbumName name =
+let CleanAlbumName name =
     let invalidChars = Array.concat [|Path.GetInvalidFileNameChars();Path.GetInvalidPathChars()|]
-
     name |> String.filter(fun c -> not(invalidChars |> Seq.exists(fun x -> x = c)))
                 
 let inline last (arr:_[]) = arr.[arr.Length - 1]
 
-let downloadAlbum albumHash targetFolder=
-    let url = sprintf "https://api.imgur.com/3/album/%s" albumHash
-    
-    let album = (Http.RequestString(url, httpMethod = "GET", headers = [ "Authorization", sprintf "Client-ID %s" clientID ])
-                 |>JsonValue.Parse).GetProperty "data"
-
-    let albumName = match (album.GetProperty "title").AsString() |> sanitizeAlbumName with
-                    |"" | null -> albumHash
-                    |name -> name
-    
+let GetImageLinksOfAlbum (album:JsonValue) =
     (album.GetProperty "images").AsArray()
-    |> Array.map(fun d -> (d.GetProperty "link").AsString())
-    |> downloadImages (Path.Combine(targetFolder, albumName))
+    |>Array.map(fun img -> (img.GetProperty "link").AsString())
+
+let GetAlbumNameOrDefault (album:JsonValue) (defaultName:string) =
+    match (album.GetProperty "title").AsString() |> CleanAlbumName with
+    |"" | null -> defaultName
+    |name -> name
+
+let DownloadAlbumData url =
+    (Http.RequestString(url, httpMethod = "GET", headers = [ "Authorization", sprintf "Client-ID %s" clientID ])
+     |>JsonValue.Parse).GetProperty "data"
+
+let downloadAlbum albumHash targetFolder=
+    let album = sprintf "https://api.imgur.com/3/album/%s" albumHash |> DownloadAlbumData    
+    GetImageLinksOfAlbum album |> DownloadLinks (Path.Combine(targetFolder, GetAlbumNameOrDefault album albumHash))
         
 let albums = File.ReadAllLines @"c:\temp\tdd.txt"
              |> Array.filter(fun line -> not(line.StartsWith "#") && not(System.String.IsNullOrWhiteSpace line))
