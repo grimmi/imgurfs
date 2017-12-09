2834f3816dfec544c0a336020e18756cf13917af
Arne Lehmann <arne.lehmann@gmx.de>
2017-12-09 02:09:24 +0100

    refactored almost all functions to have a better flow

--- ImgurFS/Script.fsx
+++ ImgurFS/Script.fsx
@@ -1,53 +1,54 @@
-﻿open FSharp.Data
-#r @"F:\Git Repos\imgurfs\packages\FSharp.Data.2.4.3\lib\net45\FSharp.Data.dll"
+﻿#r @"F:\Git Repos\imgurfs\packages\FSharp.Data.2.4.3\lib\net45\FSharp.Data.dll"
 open FSharp.Data
-open System
 open System.IO
 
 let clientID = File.ReadAllLines @"c:\temp\tdd.txt" |> Seq.head |> String.filter(fun c -> not(c.Equals('#')))
+let invalidChars = Array.concat [|Path.GetInvalidFileNameChars();Path.GetInvalidPathChars()|]
 
-let SaveStreamFromHttpResponse (targetPath:string) (fileName:string) (index:int) (response:HttpResponseWithStream) =
-    let name = sprintf "%03i - %s" index (Array.last (fileName.Split('/')))
-    let targetFile = Path.Combine(targetPath, name)
-    use saveStream = File.Create(targetFile)
+let SaveStreamFromHttpResponse (filePath:string) (response:HttpResponseWithStream) =
+    use saveStream = File.Create(filePath)
     response.ResponseStream.CopyTo(saveStream)
-    printfn "%s" targetFile
-
-let DownloadLinks targetPath (images : seq<string>) =
-    
-    if not (Directory.Exists targetPath) then (Directory.CreateDirectory(targetPath) |> ignore)
-
-    let imageLength = Seq.length images
-    let mutable counter = 1
-
-    images
-    |>Seq.map(fun image -> (image, Http.RequestStream(image, httpMethod = "GET")))
-    |>Seq.iter(fun (image, stream) -> printf "[%d / %d] " counter imageLength
-                                      SaveStreamFromHttpResponse targetPath image counter stream
-                                      counter <- counter + 1)
 
 let CleanAlbumName name =
-    let invalidChars = Array.concat [|Path.GetInvalidFileNameChars();Path.GetInvalidPathChars()|]
     name |> String.filter(fun c -> not(invalidChars |> Seq.exists(fun x -> x = c)))
                 
 let inline last (arr:_[]) = arr.[arr.Length - 1]
 
 let GetImageLinksOfAlbum (album:JsonValue) =
-    (album.GetProperty "images").AsArray()
-    |>Array.map(fun img -> (img.GetProperty "link").AsString())
+    (album, (album.GetProperty "images").AsArray()
+             |>Array.map(fun img -> (img.GetProperty "link").AsString()))
 
-let GetAlbumNameOrDefault (album:JsonValue) (defaultName:string) =
+let GetAlbumNameOrId (album:JsonValue) =
     match (album.GetProperty "title").AsString() |> CleanAlbumName with
-    |"" | null -> defaultName
+    |"" | null -> (album.GetProperty "id").AsString()
     |name -> name
 
+let MakeDownloadPath targetPath (album : JsonValue) =
+    let downloadPath = Path.Combine(targetPath, (GetAlbumNameOrId album))
+    if not (Directory.Exists downloadPath) then (Directory.CreateDirectory(downloadPath) |> ignore)
+    downloadPath
+
+let MakeImageName (link:string) (index:int) =
+    sprintf "%03i - %s" index (Array.last (link.Split('/')))
+
+let DownloadImages targetPath (albumLinks : JsonValue * string[]) =
+    let album, links = albumLinks
+    let downloadFolder = MakeDownloadPath targetPath album
+    let mutable counter = 1
+    links |>Seq.iter(fun link -> let response = Http.RequestStream(link, httpMethod = "GET")
+                                 let imagePath = Path.Combine(downloadFolder, (MakeImageName link counter))
+                                 printfn "[%d / %d] %s" counter links.Length imagePath
+                                 SaveStreamFromHttpResponse imagePath response
+                                 counter <- counter + 1)
 let DownloadAlbumData url =
     (Http.RequestString(url, httpMethod = "GET", headers = [ "Authorization", sprintf "Client-ID %s" clientID ])
      |>JsonValue.Parse).GetProperty "data"
 
 let downloadAlbum albumHash targetFolder=
-    let album = sprintf "https://api.imgur.com/3/album/%s" albumHash |> DownloadAlbumData    
-    GetImageLinksOfAlbum album |> DownloadLinks (Path.Combine(targetFolder, GetAlbumNameOrDefault album albumHash))
+    sprintf "https://api.imgur.com/3/album/%s" albumHash 
+    |> DownloadAlbumData
+    |> GetImageLinksOfAlbum
+    |> DownloadImages targetFolder
         
 let albums = File.ReadAllLines @"c:\temp\tdd.txt"
              |> Array.filter(fun line -> not(line.StartsWith "#") && not(System.String.IsNullOrWhiteSpace line))
@@ -55,4 +56,4 @@ let albums = File.ReadAllLines @"c:\temp\tdd.txt"
              |> Array.indexed 
 albums
 |> Array.iter(fun (idx, album) -> printfn "downloading %s... (%d / %d)" album (idx + 1) albums.Length 
-                                  downloadAlbum album @"c:\temp\fsdownloader\")
+                                  downloadAlbum album @"c:\temp\fsdownloadr\")
