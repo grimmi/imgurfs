f5ada74d9f9e56cf1dfdcdf623a21d3664aa5203
Arne Lehmann <arne.lehmann@gmx.de>
2017-12-30 15:40:51 +0100

    starting to add image-download functionality

---
+++ ImgurFS/ImageDownloader.fs
@@ -0,0 +1,17 @@
+﻿namespace ImgurFS
+
+open FSharp.Data
+open System
+open System.IO
+
+module ImageDownloader = 
+      
+    let clientID = File.ReadAllLines @"c:\temp\tdd.txt" |> Seq.head |> String.filter(fun c -> not(c.Equals('#')))
+
+    let DownloadImageFromHash targetFolder imageHash =
+        let image = sprintf "https://api.imgur.com/3/image/%s" imageHash
+                    |> Http.RequestStream
+        
+        let targetPath = Path.Combine(targetFolder, imageHash + ".jpg")
+
+        
