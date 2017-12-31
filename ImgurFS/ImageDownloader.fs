4189dcf75f90fd6e5549c4b4f465e8e7634865e7
Arne Lehmann <arne.lehmann@gmx.de>
2017-12-31 12:35:16 +0100

    implemented image download from imgur

--- ImgurFS/ImageDownloader.fs
+++ ImgurFS/ImageDownloader.fs
@@ -9,9 +9,19 @@ module ImageDownloader =
     let clientID = File.ReadAllLines @"c:\temp\tdd.txt" |> Seq.head |> String.filter(fun c -> not(c.Equals('#')))
 
     let DownloadImageFromHash targetFolder imageHash =
-        let image = sprintf "https://api.imgur.com/3/image/%s" imageHash
-                    |> Http.RequestStream
-        
-        let targetPath = Path.Combine(targetFolder, imageHash + ".jpg")
+        let url = sprintf "https://api.imgur.com/3/image/%s" imageHash
+        let imageData = (Http.RequestString(url, httpMethod = "GET", headers = [ "Authorization", sprintf "Client-ID %s" clientID ])
+                        |>JsonValue.Parse).GetProperty "data"
 
+        let imageUrl = (imageData.GetProperty "link").AsString()
+
+        let image = Http.RequestStream(imageUrl, httpMethod = "GET")
+
+        let imageName = Array.last(imageUrl.Split('/'))
+        let imagePath = Path.Combine(targetFolder, imageName)
+
+        use imageStream = File.Create(imagePath)
+        image.ResponseStream.CopyTo(imageStream)
+
+       
         
