83b39e8c49ec9d0f43400b6a70f1316c30080dc8
Arne Lehmann <arne.lehmann@gmx.de>
2018-01-01 11:05:17 +0100

    added a viewmodel for image downloads

---
+++ DownloaderHost/ImageDownloadViewModel.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.ComponentModel;
+using System.Threading.Tasks;
+
+namespace DownloaderHost
+{
+    public class ImageDownloadViewModel : DownloadViewModel, INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private string _imageHash;
+        public string ImageHash
+        {
+            get { return _imageHash; }
+            set
+            {
+                _imageHash = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(ImageHash)));
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Title)));
+            }
+        }
+
+        private bool _downloading;
+        public bool Downloading
+        {
+            get { return _downloading; }
+            set
+            {
+                _downloading = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Downloading)));
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Title)));
+            }
+        }
+
+        private string _imageName;
+        public string ImageName
+        {
+            get { return _imageName; }
+            set
+            {
+                _imageName = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(ImageName)));
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Title)));
+            }
+        }
+
+        public override string DownloadHash => ImageHash;
+
+        public override string Title => string.IsNullOrWhiteSpace(ImageName) ? ImageHash : ImageName;
+
+        private Task<string> _downloadTask;
+        
+        public ImageDownloadViewModel(string imageHash, Task<string> downloadTask, Action<ImageDownloadViewModel> onComplete = null)
+        {
+            ImageHash = imageHash;
+            Downloading = true;
+            _downloadTask = downloadTask;
+            _downloadTask.ContinueWith(dlTask => { ImageName = dlTask.Result; Downloading = false; onComplete?.Invoke(this); });
+        }
+
+        public override string GetPath()
+        {
+            return ImageName;
+        }
+    }
+}
